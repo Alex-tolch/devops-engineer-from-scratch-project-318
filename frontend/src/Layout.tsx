a4e758dbbd698fc19401d09fe0ab19dce3710b07
Alex-tolch <tolch1999@mail.ru>
2026-08-13 13:11:30 +0500

    delete all app data

--- frontend/src/Layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import { Layout as RALayout } from "react-admin";
-
-export const Layout = ({ children }: { children: ReactNode }) => (
-  <RALayout>{children}</RALayout>
-);
